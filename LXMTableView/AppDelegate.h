1a7f4db1fc228402f861c73e915b7ff57124a05f
lixu <lixu@rd.netease.com>
2016-01-15 19:36:38 +0800

    Initial Commit

---
+++ LXMTableView/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  LXMTableView
+//
+//  Created by lixu on 1/15/16.
+//  Copyright © 2016 lixu. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ LXMTableView/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  LXMTableView
+//
+//  Created by lixu on 1/15/16.
+//  Copyright © 2016 lixu. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
